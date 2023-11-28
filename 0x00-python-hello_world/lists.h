76f9af44ebfa00dc928b1ba112a67fca9549993e
Chidubemkingsley <rubelu2017@gmail.com>
2023-11-28 02:19:18 -0800

    102 wall street

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -21,5 +21,6 @@ size_t print_listint(const listint_t *h);
 void free_listint(listint_t *head);
 listint_t *add_nodeint(listint_t **head, const int n);
 int check_cycle(listint_t *list);
+def magic_calculation(a, b):
 
 #endif /* LISTS_H */
