3107c0592a503f3edb476673ccfb6947252ac97e
Chidubemkingsley <rubelu2017@gmail.com>
2024-02-15 17:07:14 -0800

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -2,4 +2,4 @@
 -- table second_table of the current database.
 -- Result should display both the score and the name, ordered by score.
 
-SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DES;
+SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DESC;
